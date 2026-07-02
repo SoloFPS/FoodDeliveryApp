f193a97937d6562e78d4c246b6ff579924d1fd60
User Name <user@gmail.com>
2026-07-02 22:06:22 +0530

    Use Render port

--- Backend/server.js
+++ Backend/server.js
@@ -10,6 +10,8 @@ import orderRouter from './routes/orderRoutes.js'
 //app config
 const app = express()
 
+const PORT = process.env.PORT || 4000
+
 //middlewares
 app.use(express.json())
 
@@ -28,6 +30,6 @@ app.get('/', (req, res, next) => {
     res.send("API IS WORKING")
 })
 
-app.listen(process.env.PORT || 4000, () => {
+app.listen(PORT || 4000, () => {
     console.log(`The server is running on port http://localhost:${port}`)
 })
