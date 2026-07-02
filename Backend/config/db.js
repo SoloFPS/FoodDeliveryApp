80da7b40defcbda497569073f0ef1d605d01c67f
User Name <user@gmail.com>
2026-07-02 22:08:48 +0530

    fixed mongo URI

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb://Prithvi:Prithvi0607@ac-yx9ljf3-shard-00-00.olgpt0o.mongodb.net:27017,ac-yx9ljf3-shard-00-01.olgpt0o.mongodb.net:27017,ac-yx9ljf3-shard-00-02.olgpt0o.mongodb.net:27017/?ssl=true&replicaSet=atlas-yhu4q2-shard-0&authSource=admin&appName=foodapp').then(() => { console.log('DB connected') })
+    await mongoose.connect(process.env.MONGO_URI).then(() => { console.log('DB connected') })
 }
